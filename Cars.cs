b3c7769ec680f07a8bb360e6f7dca6507446d37d
Chris Wingo and Stephan Khoury <chris+stephan@>
2016-11-14 10:20:37 -0800

    Add a default message if no cars are returned from the search

--- Cars.cs
+++ Cars.cs
@@ -6,6 +6,7 @@ class Car
   private string _makeModel;
   private int _price;
   private int _miles;
+  private string _description;
 
   public void SetMakeModel(string newMakeModel)
   {
@@ -34,11 +35,21 @@ class Car
     return _miles;
   }
 
-  public Car(string newMakeModel, int newPrice, int newMiles)
+  public void SetDescription(string newDescription)
+  {
+    _description = newDescription;
+  }
+  public string GetDescription()
+  {
+    return _description;
+  }
+
+  public Car(string newMakeModel, int newPrice, int newMiles, string newDescription)
   {
     SetMakeModel(newMakeModel);
     SetPrice(newPrice);
     SetMiles(newMiles);
+    SetDescription(newDescription);
   }
 }
 
@@ -46,20 +57,21 @@ public class Program
 {
   public static void Main()
   {
-    Car porsche = new Car("2014 Porsche 911", 114991, 7864);
+    Car porsche = new Car("2014 Porsche 911", 114991, 7864, "Like the fastest black toad you've ever seen.");
 
-    Car ford = new Car("2011 Ford F450", 55995, 14241);
+    Car ford = new Car("2011 Ford F450", 55995, 14241, "Perfect for hauling all your big shit.");
 
-    Car lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+    Car lexus = new Car("2013 Lexus RX 350", 44700, 20000, "Perfect 'Mom' car.");
 
-    Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
+    Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "Affordable old-man car.");
 
-    Car bmw = new Car("BMW X1", 20000, 0);
+    Car bmw = new Car("BMW X1", 20000, 0, "Sporty, young, you know you want it.");
 
     List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes, bmw };
     foreach(Car automobile in Cars)
     {
       Console.WriteLine(automobile.GetMakeModel());
+      Console.WriteLine(automobile.GetDescription());
     }
 
     Console.WriteLine("Enter maximum price: ");
@@ -83,6 +95,11 @@ public class Program
     foreach(Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.GetMakeModel());
+    }
+
+    if(CarsMatchingSearch.Count == 0)
+    {
+      Console.WriteLine("Sorry, No cars match your search paramaters.");
     }
   }
 }
